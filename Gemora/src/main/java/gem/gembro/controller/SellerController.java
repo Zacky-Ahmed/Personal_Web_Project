e3221cd270521845825681acaf6e1965c5b3b0f4
IT24102553 <it24102553@my.sliit.lk>
2025-11-17 12:22:26 +0530

    updates

--- Gemora/src/main/java/gem/gembro/controller/SellerController.java
+++ Gemora/src/main/java/gem/gembro/controller/SellerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/sellers")
@@ -151,4 +152,5 @@ public class SellerController {
         response.put("user", user);
         return ResponseEntity.ok(response);
     }
+}
 
