6fa598bd2967dbe7983f6a034d51f929cc05e107
IT24102553 <it24102553@my.sliit.lk>
2025-11-17 10:05:49 +0530

    updates

---
+++ Gemora/src/main/java/gem/gemora/GemoraApplication.java
@@ -0,0 +1,13 @@
+package gem.gemora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GemoraApplication.class, args);
+    }
+
+}
